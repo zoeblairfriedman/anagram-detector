7c0ed30eb4c499f6ac13aafc3a5f895839aa008c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-19 12:35:51 -0500

    commit tree 5b7f540e5e197b988c06658b65b68985e42afa37

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+ attr_accessor :word, :array
+
+def initialize(word)
+@word = word
+@array = array
+end
+
+def match(array)
+a = @word.split("").sort
+array_of_anagrams = []
+array.each do |word_to_check|
+    if word_to_check.split("").sort == a 
+        array_of_anagrams << word_to_check
+     end
+end
+array_of_anagrams
+end
+
+end
